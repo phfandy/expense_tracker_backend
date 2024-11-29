0c79f3508b04439d26c99fbcef6302097d66e9db
phfandy <phfungandy@gmail.com>
2024-11-29 18:14:48 -0500

    comit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const AuthRouter = require('./Routes/AuthRouter');
 const ProductRouter = require('./Routes/ProductRouter');
+const ExpenseRouter = require('./Routes/ExpenseRouter');
+const ensureAuthenticated = require('./Middlewares/Auth');
 
 require('dotenv').config();
 require('./Models/db');
@@ -15,9 +17,10 @@ app.get('/ping', (req, res) => {
 
 app.use(bodyParser.json());
 app.use(cors());
+
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
-
+app.use('/expenses', ensureAuthenticated, ExpenseRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
